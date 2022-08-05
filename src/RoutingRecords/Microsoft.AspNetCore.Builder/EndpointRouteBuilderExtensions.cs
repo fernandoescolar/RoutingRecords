7a4e06fdbf676d5ed14a7f7337ae74cdea69e0d6
Fernando Escolar <fernando.escolar@scrm.lidl>
2022-08-05 10:44:52 +0200

    some type fixes

--- src/RoutingRecords/Microsoft.AspNetCore.Builder/EndpointRouteBuilderExtensions.cs
+++ src/RoutingRecords/Microsoft.AspNetCore.Builder/EndpointRouteBuilderExtensions.cs
@@ -9,7 +9,7 @@ public static class EndpointRouteBuilderExtensions
     /// </summary>
     /// <param name="endpoints">The <see cref="IEndpointRouteBuilder" /> to add the route to.</param>
     /// <returns>A <see cref="IRecordEndpointConventionBuilderCollection" /> that can be used to further customize the endpoints.</returns>
-    public static IEndpointConventionBuilder MapRouteRecords(this IEndpointRouteBuilder endpoints)
+    public static IRecordEndpointConventionBuilderCollection MapRouteRecords(this IEndpointRouteBuilder endpoints)
     {
         using var scope = endpoints.ServiceProvider.CreateScope();
         var builders = scope.ServiceProvider
@@ -25,7 +25,7 @@ public static class EndpointRouteBuilderExtensions
     /// <param name="endpoints">The <see cref="IEndpointRouteBuilder" /> to add the route to.</param>
     /// <param name="route">The <see cref="RouteRecord" /> to add.</param>
     /// <returns>A <see cref="IRecordEndpointConventionBuilder" /> that can be used to further customize the endpoint.</returns>
-    public static IEndpointConventionBuilder MapRouteRecord(this IEndpointRouteBuilder endpoints, RouteRecord route)
+    public static IRecordEndpointConventionBuilder MapRouteRecord(this IEndpointRouteBuilder endpoints, RouteRecord route)
     {
         using var scope = endpoints.ServiceProvider.CreateScope();
         return endpoints.MapRouteRecord(scope.ServiceProvider, route);
@@ -37,7 +37,7 @@ public static class EndpointRouteBuilderExtensions
     /// <typeparam name="T">The <see cref="RouteRecord" /> type to add.</typeparam>
     /// <param name="endpoints">The <see cref="IEndpointRouteBuilder" /> to add the route to.</param>
     /// <returns>A <see cref="IRecordEndpointConventionBuilder" /> that can be used to further customize the endpoint.</returns>
-    public static IEndpointConventionBuilder MapRouteRecord<T>(this IEndpointRouteBuilder endpoints) where T : RouteRecord
+    public static IRecordEndpointConventionBuilder MapRouteRecord<T>(this IEndpointRouteBuilder endpoints) where T : RouteRecord
         => endpoints.MapRouteRecord(typeof(T));
 
     /// <summary>
@@ -46,7 +46,7 @@ public static class EndpointRouteBuilderExtensions
     /// <param name="endpoints">The <see cref="IEndpointRouteBuilder" /> to add the route to.</param>
     /// <param name="type">The <see cref="RouteRecord" /> type to add.</typeparam>
     /// <returns>A <see cref="IRecordEndpointConventionBuilder" /> that can be used to further customize the endpoint.</returns>
-    public static IEndpointConventionBuilder MapRouteRecord(this IEndpointRouteBuilder endpoints, Type type)
+    public static IRecordEndpointConventionBuilder MapRouteRecord(this IEndpointRouteBuilder endpoints, Type type)
     {
         if (!typeof(RouteRecord).IsAssignableFrom(type))
         {
@@ -59,17 +59,18 @@ public static class EndpointRouteBuilderExtensions
         return endpoints.MapRouteRecord(scope.ServiceProvider, route);
     }
 
-    private static IEndpointConventionBuilder MapRouteRecord(this IEndpointRouteBuilder endpoints, IServiceProvider serviceProvider, RouteRecord route)
-    { 
+    private static IRecordEndpointConventionBuilder MapRouteRecord(this IEndpointRouteBuilder endpoints, IServiceProvider serviceProvider, RouteRecord route)
+    {
         var requestDelegate = CreateDelegate(serviceProvider, route);
         var builder = endpoints.Map(RoutePatternFactory.Parse(route.Pattern), requestDelegate);
         builder.WithDisplayName($"{ route.Pattern } HTTP: { route.Verb }");
         builder.WithMetadata(new HttpMethodMetadata(new[] { route.Verb }));
         builder.WithMetadata(route.Delegate.GetMethodInfo());
         builder.WithMetadata(route.GetType().GetCustomAttributes(true));
-        return builder;
+
+        return new RecordEndpointConventionBuilder(route.GetType(), builder);
     }
-        
+
 
     private static RequestDelegate CreateDelegate(IServiceProvider serviceProvider, RouteRecord route)
     {
--- src/RoutingRecords/Microsoft.AspNetCore.Builder/RecordEndpointConventionBuilderCollection.cs
+++ src/RoutingRecords/Microsoft.AspNetCore.Builder/RecordEndpointConventionBuilderCollection.cs
@@ -2,17 +2,17 @@ namespace Microsoft.AspNetCore.Builder;
 
 internal class RecordEndpointConventionBuilderCollection : IRecordEndpointConventionBuilderCollection
 {
-    private readonly List<IEndpointConventionBuilder> _innerBuilders;
+    private readonly List<IRecordEndpointConventionBuilder> _innerBuilders;
 
-    public RecordEndpointConventionBuilderCollection(IEnumerable<IEndpointConventionBuilder> innerBuilders)
+    public RecordEndpointConventionBuilderCollection(IEnumerable<IRecordEndpointConventionBuilder> innerBuilders)
     {
-        _innerBuilders = new List<IEndpointConventionBuilder>(innerBuilders);
+        _innerBuilders = new List<IRecordEndpointConventionBuilder>(innerBuilders);
     }
 
     public void Add(Action<EndpointBuilder> convention)
         => _innerBuilders.ForEach(x => x.Add(convention));
 
-    public IEnumerator<IEndpointConventionBuilder> GetEnumerator()
+    public IEnumerator<IRecordEndpointConventionBuilder> GetEnumerator()
         => _innerBuilders.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator()
