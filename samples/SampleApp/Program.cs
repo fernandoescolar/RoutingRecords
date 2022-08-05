c7e0aea96ef125197ab72093c87f9b432d75dc6e
fernandoescolar <f.escolar@hotmail.com>
2022-08-05 10:50:35 +0200

    show swagger always in SampleApp

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -5,13 +5,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 app.MapRouteRecords();
-
 
 app.Run();
